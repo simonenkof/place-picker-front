3c120fb4b3fbc63145831a9d557853cdd6ea4d37
Simonenko Fedor <simonenko.f.a@gmail.com>
2025-10-02 22:06:51 +0300

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/EW0tephYT3

---
+++ apps/place-picker-front/app/app.tsx
@@ -0,0 +1,13 @@
+// Uncomment this line to use CSS modules
+// import styles from './app.module.scss';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@place-picker-front/place-picker-front" />
+    </div>
+  );
+}
+
+export default App;
